b5fb467a9266b3b8624daa48cd6cacc80b7513a3
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-05-12 16:04:01 +0530

    added env file to the project

--- src/helpers/singleProduct/fetchSingleProduct.js
+++ src/helpers/singleProduct/fetchSingleProduct.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchSingleProduct = async (productID) => {
     try {
-        const response = await axios.get(`/api/products/${productID}`);
+        const response = await axios.get(`${process.env.REACT_APP_API_HOST_URL}/products/${productID}`);
         return response.data
     } catch (error) {
         console.error(error)
--- src/utils/api/fetchProducts.js
+++ src/utils/api/fetchProducts.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const fetchProducts = async () => {
     try {
-        const res = await axios.get('/api/products');
+        const res = await axios.get(`${process.env.REACT_APP_API_HOST_URL}/products`);
         return res.data.products;
     } catch (e) {
-        console.error("error ",e)
+        console.error("error ", e)
     }
 }
 
