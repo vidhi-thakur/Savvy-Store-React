6d259c850c027396f313e36d2ecad0f16c253a32
Vidhi Thakur <72219676+vidhi-thakur@users.noreply.github.com>
2022-05-15 22:28:56 +0530

    updated database to add more products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -36,4 +36,214 @@ export const products = [
     description: "6pc Mini Fairies Mini Garden Fairy Garden Accessories", 
     productImage: "https://i.etsystatic.com/7202160/r/il/ad8ca4/1146706082/il_340x270.1146706082_1ior.jpg"
   },
+  {
+    _id: uuid(),
+    title: "Cactus planter",
+    author: "SFauteuxCeramiste",
+    price:  1864,
+    categoryName: "Vintage",
+    rating: 5, 
+    description: "Hanging cactus planter - Pink - Fruit and coniferous trees", 
+    productImage: "https://i.etsystatic.com/9522335/r/il/8c2d5a/2628482337/il_794xN.2628482337_kzwx.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "DIY Macrame Kit",
+    author: "MBcordas",
+    price: 834,
+    categoryName: "Handmade",
+    rating: 4, 
+    description: "Complete Boho Decor Macrame Plant Hanger Kit with 100% Cotton Macrame Cord", 
+    productImage: "https://i.etsystatic.com/13326012/r/il/6dc8aa/3869603068/il_794xN.3869603068_l88i.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Natural Jute Twine",
+    author: "leboxboutique",
+    price: 606,
+    categoryName: "Handmade",
+    rating: 2, 
+    description: "Natural Jute Twine 65 yards (195 ft) | Home & Garden Supply | Decorative Twine", 
+    productImage: "https://i.etsystatic.com/5770799/r/il/a177af/263095670/il_794xN.263095670.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Indoor plant pot",
+    author: "JOceramic",
+    price: 1475,
+    categoryName: "Handmade",
+    rating: 2, 
+    description: "A handmade yellow and navy blue indoor plant pot", 
+    productImage: "https://i.etsystatic.com/17435949/r/il/004312/3923627271/il_794xN.3923627271_f3gv.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Falcon Concrete Planter",
+    author: "AnsonDesign",
+    price: 1257,
+    categoryName: "Handmade",
+    rating: 5, 
+    description: "Small Millennium Falcon Concrete Planter - Includes Air Plant - Paperweight - Star Wars", 
+    productImage: "https://i.etsystatic.com/7027077/r/il/5f5a83/926502753/il_794xN.926502753_8onj.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Macrame Plant Hanger",
+    author: "HuiswerkStore",
+    price: 1771,
+    categoryName: "Handmade",
+    rating: 4, 
+    description: "The Spiral Crown Macrame Plant Hanger // Natural Cotton // in 2 sizes", 
+    productImage: "https://i.etsystatic.com/12551773/r/il/d6310c/1233947646/il_794xN.1233947646_tlp7.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Antique French Planter Pot",
+    author: "Gadgetalicious",
+    price: 24967,
+    categoryName: "Vintage",
+    rating: 4, 
+    description: "French Huge Brass Planter Pot, Antique Plant Pot Bowl, Lion Handles", 
+    productImage: "https://i.etsystatic.com/26397628/r/il/cadecf/3922689443/il_794xN.3922689443_7thn.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Elf Pixie Planter",
+    author: "TreasuryShop",
+    price: 2085,
+    categoryName: "Vintage",
+    rating: 5, 
+    description: "Green Leprechaun / Elf Pixie Planter with a Cello / Musical Instrument", 
+    productImage: "https://i.etsystatic.com/6319497/r/il/858f0b/1094399328/il_794xN.1094399328_azco.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Petrified rock stone mineral",
+    author: "EvasFeathers",
+    price: 557,
+    categoryName: "Vintage",
+    rating: 3, 
+    description: "Fossil Sand dollars petrified rock stone mineral specimen cab cabochon cabachon", 
+    productImage: "https://i.etsystatic.com/6536125/r/il/18f588/567295665/il_794xN.567295665_ef2h.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Sanmyro Vintage Planter",
+    author: "RandysGallery",
+    price: 1760,
+    categoryName: "Vintage",
+    rating: 3, 
+    description: "Vintage Planter, Ceramic vintage from the 1950s", 
+    productImage: "https://i.etsystatic.com/8366410/r/il/3a2f88/724478720/il_794xN.724478720_77da.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Frenchfoot warmer",
+    author: "EuropeanBrocante",
+    price: 760,
+    categoryName: "Vintage",
+    rating: 2, 
+    description: "Antique French cast iron foot warmer - Vintage garden", 
+    productImage: "https://i.etsystatic.com/15656876/r/il/85dbbb/2196043917/il_794xN.2196043917_6g7g.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Ceramic Cat Planter",
+    author: "ItsThatVintageStore",
+    price: 6805,
+    categoryName: "Vintage",
+    rating: 3, 
+    description: "RARE Lefton's Mid-century Ceramic Cat Planter Blue With Pink Bow", 
+    productImage: "https://i.etsystatic.com/9513420/r/il/ad3c85/3923520409/il_794xN.3923520409_qujx.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "ROBOT PLANTERS",
+    author: "Beau2Bear",
+    price: 3300,
+    categoryName: "Vintage",
+    rating: 2, 
+    description: "Decorative, quirky figurine, pottery, Ceramics, gift for friend", 
+    productImage: "https://i.etsystatic.com/11241029/r/il/2bb215/796241568/il_794xN.796241568_9l5d.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Milk Glass Vases",
+    author: "StickAPlantInIt",
+    price: 1757,
+    categoryName: "Vintage",
+    rating: 5, 
+    description: "Trio of Vintage Milk Glass Vases for water propagation", 
+    productImage: "https://i.etsystatic.com/5935733/r/il/b8c84d/3923489815/il_794xN.3923489815_csp6.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Hanging pot cover",
+    author: "kilimpillowkingdom",
+    price: 240,
+    categoryName: "Handmade",
+    rating: 1, 
+    description: "Basket for decor, hanging plant pot, round planter", 
+    productImage: "https://i.etsystatic.com/21445821/r/il/8938be/3874436110/il_794xN.3874436110_itgr.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Indoor planters",
+    author: "MinimumDesign",
+    price: 4235,
+    categoryName: "Handmade",
+    rating: 5, 
+    description: "Set of 4 small indoor planters - Original planter gift!", 
+    productImage: "https://i.etsystatic.com/13346155/r/il/c4458b/1418769174/il_794xN.1418769174_3xuk.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Hanging terrarium",
+    author: "PlantsinMind",
+    price: 2299,
+    categoryName: "Handmade",
+    rating: 5, 
+    description: "Succulent terrarium/hanging planter/succulent garden", 
+    productImage: "https://i.etsystatic.com/10364512/r/il/a12176/1058997736/il_794xN.1058997736_l1lb.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Handmade ceramic planter",
+    author: "artcrafthome",
+    price: 4342,
+    categoryName: "Handmade",
+    rating: 4, 
+    description: "Simple shape, modern design, beautiful addition to your indoor or outdoor spaces", 
+    productImage: "https://i.etsystatic.com/6426954/r/il/65a375/794693722/il_794xN.794693722_3wtr.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Macrame plant hanger",
+    author: "EarthChakraGems",
+    price: 1224,
+    categoryName: "Handmade",
+    rating: 2, 
+    description: "Macramé plant hanger with a wavy tassel -  boho decor, bohemian, rustic", 
+    productImage: "https://i.etsystatic.com/14078112/r/il/9cc114/3065174653/il_794xN.3065174653_dws0.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Cat planter",
+    author: "theYarnKitchen",
+    price: 257,
+    categoryName: "Handmade",
+    rating: 1, 
+    description: "Siamese cat, a sleepy cat head, perfect gift for a cat lover", 
+    productImage: "https://i.etsystatic.com/6193770/r/il/50c4e5/3675349522/il_794xN.3675349522_7hte.jpg"
+  },
+  {
+    _id: uuid(),
+    title: "Terrariums Centerpieces Bowl",
+    author: "PrefleurMoss",
+    price: 3420,
+    categoryName: "Handmade",
+    rating: 3, 
+    description: "Cushion Moss Natural Green Decorative Real Terrariums Centerpieces Bowl", 
+    productImage: "https://i.etsystatic.com/21599334/r/il/58c2e7/3154479745/il_340x270.3154479745_9r4w.jpg"
+  },
 ];
